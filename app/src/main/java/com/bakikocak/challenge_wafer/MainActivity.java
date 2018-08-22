0001380d77cfede3554d84e99bf241631447f37f
Abdulbaki Kocak <abdulbaki.kocak@dsmart.com.tr>
2018-08-23 01:23:24 +0300

    Created project
    Created project

---
+++ app/src/main/java/com/bakikocak/challenge_wafer/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bakikocak.challenge_wafer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initialize views
+        // fetch data
+        // create arraylist for listview
+    }
+}
